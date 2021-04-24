dee5ca2242d48ed890000135a1998859e36bf433
Jake Peterson <jpeterson0514@gmail.com>
2021-04-24 16:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function doToElementsInArray(array,callback) {
   array.forEach(callback)
 }
-function changeCompletely(callback,array){
-
+function changeCompletely(element,index,array){
+ array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`
   
 }
