3924c840377c87e1cb447af4c4c691289c9a2d0b
Jake Peterson <jpeterson0514@gmail.com>
2021-04-24 15:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(callback) {
-  Array.prototype.forEach(callback)
+function doToElementsInArray(array,callback) {
+  array.prototype.forEach(callback)
 }
 
 // Add your changeCompletely() function here:
